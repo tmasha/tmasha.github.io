041228d24ff590efc40f7d8dbfd77caf2312ed1e
Thomas <thomas8483828180@gmail.com>
2023-05-22 19:46:52 -0500

    host attempt fix

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import './style.css'
-import * as THREE from '/node_modules/three/build/three.module';
+import '/style.css'
+import * as THREE from 'three';
 
 // general setup
 const scene = new THREE.Scene();
