19f9cb9e87234bbf7e6fbbd985bb372d9bd3e197
Thomas <thomas8483828180@gmail.com>
2023-05-22 20:07:00 -0500

    oh mah gawd graaa

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "/node_modules/three/build/three.module.js";
+import * as THREE from '/node_modules/three/build/three.module.js';
 
 // general setup
 const scene = new THREE.Scene();
@@ -11,8 +11,6 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.render( scene, camera );
-
-
 
 // lights
 const pointLight = new THREE.PointLight(0xfffff);
