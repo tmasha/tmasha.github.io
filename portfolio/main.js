e780b865f309c6145754b863150af4483ef2663c
Thomas <thomas8483828180@gmail.com>
2023-05-24 13:47:03 -0500

    import three

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 // general setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
